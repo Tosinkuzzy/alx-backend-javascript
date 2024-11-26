c195d9d52c3312a334d219e2aff63d4f2977f598
Tosinkuzzy <Adeleye608@gmail.com>
2024-11-26 21:04:03 +0100

    1-stdin command line control

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -11,7 +11,3 @@ process.stdin.on('readable', () => {
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
-process.stdin.on('readable', () => {
-    const chunk = process.stdout.write('Program successfully excuted ${chunk}');
-});
