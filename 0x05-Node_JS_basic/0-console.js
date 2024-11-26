001f214e42c370c8ea18b08b80e6f086f767d14c
Tosinkuzzy <Adeleye608@gmail.com>
2024-11-26 20:36:55 +0100

    console updated

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Salah Eddine Hamidi <https://github.com/gzaros>
+ * @author Adeleye Ayomide Tosin <https://github.com/tosinkuzzy>
  */
 const displayMessage = (msg) => {
   console.log(msg);
